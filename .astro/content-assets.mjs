e731d76a7f1043bf791b4b7f327dc54f47491a86
Alexandre de Abreu <alexandre.abreu@eletromidia.com.br>
2025-10-15 14:55:29 -0300

    Start

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_1K5Sm3 from "../../assets/smartruler01.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
+export default new Map([["../../assets/smartruler01.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_1K5Sm3]]);
+		
