9f3c75801b45dea9817f9358c81d848636d89877
alexabreup <eletrolabelt@gmail.com>
2025-10-16 06:48:10 -0300

    Atualizações finais: configuração Netlify e arquivos de build

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,4 @@
 
-import __ASTRO_IMAGE_IMPORT_1K5Sm3 from "../../assets/smartruler01.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
-export default new Map([["../../assets/smartruler01.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_1K5Sm3]]);
+import __ASTRO_IMAGE_IMPORT_ZdmXvO from "../../assets/smart-ruler_capa.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
+export default new Map([["../../assets/smart-ruler_capa.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_ZdmXvO]]);
 		
